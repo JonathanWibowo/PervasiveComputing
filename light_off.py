48588c189f98c284a7cb85c69641428889ecc39f
JonathanWibowo <wibowo.jonathan@ymail.com>
2017-05-09 09:25:05 +0700

    Update light_off.py

--- light_off.py
+++ light_off.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)#for GPIO numbering
+GPIO.setwarnings(False)#making warning message not appear
 GPIO.setup(18,GPIO.OUT)#set GPIO port 18 as output
 GPIO.output(18,GPIO.LOW)#turn off the lamp
